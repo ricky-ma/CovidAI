2a8ca3581e66ac217f41b95f9716b9f9541d384d
rickyma <mr.rickyma@gmail.com>
2020-04-23 15:45:01 -0700

    Transfer learning with DenseNet

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 import pickle as pk
 import numpy as np
 import copy
+from PIL import Image
 from model import Net
 
 
@@ -47,7 +48,7 @@ def make_data_loaders():
     train_dataset = CovidDatasetTrain(train_imgs, train_labels)
     test_dataset = CovidDatasetTest(test_imgs)
 
-    batch_size = 7
+    batch_size = 5
     validation_split = 0.1
     random_seed = 43
 
@@ -70,20 +71,7 @@ def make_data_loaders():
     }
 
 
-def imshow(inp, title=None):
-    """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    # mean = np.array([0.485, 0.456, 0.406])
-    # std = np.array([0.229, 0.224, 0.225])
-    # inp = std * inp + mean
-    # inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
-
-
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+def fit(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
@@ -153,13 +141,10 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     return model
 
 
-def visualize_model(model, num_images=6):
+def predict(model):
     was_training = model.training
     model.eval()
-    images_so_far = 0
-    fig = plt.figure()
     predictions = []
-
     with torch.no_grad():
         for i, (inputs) in enumerate(data_loaders['test']):
             inputs = inputs.to(device)
@@ -167,16 +152,6 @@ def visualize_model(model, num_images=6):
             outputs = model(inputs)
             _, preds = torch.max(outputs, 1)
             predictions.append(preds)
-            # for j in range(inputs.size()[0]):
-            #     images_so_far += 1
-            #     ax = plt.subplot(num_images//2, 2, images_so_far)
-            #     ax.axis('off')
-            #     ax.set_title('predicted: {}'.format(class_names[preds[j]]))
-            #     imshow(inputs.cpu().data[j])
-            #
-            #     if images_so_far == num_images:
-            #         model.train(mode=was_training)
-            #         return
         model.train(mode=was_training)
     print(predictions)
 
@@ -196,94 +171,44 @@ if __name__ == '__main__':
 
     class_names = ['covid', 'background']
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # device = "cpu"
 
-    # # Get a batch of training data
-    # inputs, classes = next(iter(data_loaders['train']))
-    # # Make a grid from batch
-    # out = torchvision.utils.make_grid(inputs)
-    # imshow(out, title=[class_names[x] for x in classes])
-    # time.sleep(60)
+    # ResNet18
+    # model_ft = models.resnet18(pretrained=True)
+    # num_ftrs = model_ft.fc.in_features
+    # model_ft.fc = nn.Linear(num_ftrs, 2)
+    # model_ft = model_ft.to(device)
+    # criterion = nn.CrossEntropyLoss()
+    # optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    #
+    # model_ft = fit(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=30)
+    # predict(model_ft)
 
 
-    model_ft = models.resnet18(pretrained=True)
+    # model_conv = torchvision.models.resnet18(pretrained=True)
+    # for param in model_conv.parameters():
+    #     param.requires_grad = False
+    # num_ftrs = model_conv.fc.in_features
+    # model_conv.fc = nn.Linear(num_ftrs, 2)
+    # model_conv = model_conv.to(device)
+    # criterion = nn.CrossEntropyLoss()
+    # optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+    #
+    # model_conv = fit(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=25)
+    # predict(model_conv)
 
-    num_ftrs = model_ft.fc.in_features
-    # Here the size of each output sample is set to 2.
-    # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-    model_ft.fc = nn.Linear(num_ftrs, 2)
+    # DenseNet161
+    model_ft = models.densenet161(pretrained=True)
+    num_ftrs = model_ft.classifier.in_features
+    model_ft.classifier = nn.Linear(num_ftrs, 2)
     model_ft = model_ft.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=30)
 
-    visualize_model(model_ft)
-
-
-    # model = Net()
-    # model.to(device)
-    #
-    # # training
-    # start_time = time.time()
-    # criterion = nn.CrossEntropyLoss()
-    # optimizer = optim.SGD(model.parameters(), lr=0.0005, momentum=0.9)
-    # for epoch in range(10):  # loop over the dataset multiple times
-    #     running_loss = 0.0
-    #     for i, data in enumerate(data_loaders['train'], 0):
-    #         # get the inputs; data is a list of [inputs, labels]
-    #         inputs, labels = data[0].to(device), data[1].to(device)
-    #
-    #         # zero the parameter gradients
-    #         optimizer.zero_grad()
-    #
-    #         # forward + backward + optimize
-    #         outputs = model(inputs)
-    #         loss = criterion(outputs, labels)
-    #         loss.backward()
-    #         optimizer.step()
-    #
-    #         # print statistics
-    #         running_loss += loss.item()
-    #         if i % 5 == 4:  # print every 2000 mini-batches
-    #             print('[%d, %5d] loss: %.3f' %
-    #                   (epoch + 1, i + 1, running_loss / 5))
-    #             running_loss = 0.0
-    # print('Finished Training')
-    # print('Trained for: ' + str(time.time()-start_time))
-    #
-    # # prediction on validations
-    # dataiter = iter(data_loaders['validation'])
-    # images, labels = dataiter.next()
-    #
-    # correct = 0
-    # total = 0
-    # predictions = []
-    # with torch.no_grad():
-    #     for data in data_loaders['validation']:
-    #         images, labels = data[0].to(device), data[1].to(device)
-    #         outputs = model(images)
-    #         _, predicted = torch.max(outputs.data, 1)
-    #         total += labels.size(0)
-    #         correct += (predicted == labels).sum().item()
-    #         predictions.append(predicted)
-    # print(predictions)
-    # print('Accuracy of the network on the validation images: %d %%' % (100 * correct / total))
-    #
-    # # prediction on test
-    # model.eval()
-    # dataiter = iter(data_loaders['test'])
-    # images = dataiter.next()
-    #
-    # predictions = []
-    # with torch.no_grad():
-    #     for data in data_loaders['test']:
-    #         images = data.to(device)
-    #         outputs = model(images)
-    #         _, predicted = torch.max(outputs.data, 1)
-    #         predictions.append(predicted)
-    # print(predictions)
-
+    model_ft = fit(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+    predict(model_ft)
 
 
 
